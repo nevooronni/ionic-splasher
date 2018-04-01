ff2092f49260cd3f264485087596b29b74eb3ef1
Neville Oronni <nevooronni@gmail.com>
2018-04-01 03:47:29 +0300

    added splash screen animation and templates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  //properties
+  splash = true;
+  tabBarElement: any;
 
   constructor(public navCtrl: NavController) {
-
+    this.tabBarElement = document.querySelector('.tabbar');
   }
 
+  ionViewDidLoad() {
+    //app loads tabbar is hiddden
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+    }, 4000);
+
+  }
 }
